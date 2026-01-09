27ee9818a03aa59d61f8d3c7c2dd8b016f8695a9
ananyajain2005 <161947103+ananyajain2005@users.noreply.github.com>
2026-01-09 21:33:01 +0530

    header

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,13 +8,12 @@ const Login = () => {
    const { user } = useContext(UserContext);
   const router = useRouter();
   useEffect(() => {
-    // 🔐 agar user already login hai
+    
     if (user) {
-      router.replace("/dashboard"); // back se login page nahi aayega
+      router.replace("/dashboard");
     }
   }, [user]);
   
-  // 🛑 jab tak redirect ho raha hai tab kuch render mat karo
   if (user) return null;
 
   return <><Form type="login"/></>
